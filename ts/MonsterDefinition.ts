6a3ee16ece8253521c59fd3ab219ffbea2d43b96
Ville Jungman <ville.jungman@gmail.com>
2026-08-02 03:59:17 +0300

    rebalance monster fight difficulty

--- ts/MonsterDefinition.ts
+++ ts/MonsterDefinition.ts
@@ -19,33 +19,33 @@ export class MonsterDefinition {
 
     static get(name: string): MonsterDefinition|null {
         if (name === "rat") {
-            return new MonsterDefinition("rat", 10, [
-                { damage: 2, block: 0, healing: 0 },
-                { damage: 0, block: 2, healing: 0 },
+            return new MonsterDefinition("rat", 7, [
+                { damage: 1, block: 0, healing: 0 },
+                { damage: 0, block: 1, healing: 0 },
                 { damage: 0, block: 0, healing: 0 },
-                { damage: 2, block: 0, healing: 1 },
-                { damage: 4, block: 0, healing: 0 },
+                { damage: 1, block: 0, healing: 1 },
+                { damage: 3, block: 0, healing: 0 },
             ], 5);
         }
         if (name === "orc") {
-            return new MonsterDefinition("orc", 24, [
-                { damage: 4, block: 1, healing: 0 },
-                { damage: 0, block: 4, healing: 0 },
-                { damage: 0, block: 0, healing: 3 },
+            return new MonsterDefinition("orc", 18, [
+                { damage: 3, block: 1, healing: 0 },
+                { damage: 0, block: 3, healing: 0 },
+                { damage: 0, block: 0, healing: 2 },
                 { damage: 0, block: 0, healing: 0 },
-                { damage: 5, block: 3, healing: 0 },
-                { damage: 7, block: 0, healing: 0 },
+                { damage: 4, block: 2, healing: 0 },
+                { damage: 5, block: 0, healing: 0 },
             ], 5);
         }
         if (name === "troll") {
-            return new MonsterDefinition("troll", 45, [
-                { damage: 6, block: 0, healing: 0 },
-                { damage: 0, block: 6, healing: 3 },
+            return new MonsterDefinition("troll", 34, [
+                { damage: 5, block: 0, healing: 0 },
+                { damage: 0, block: 4, healing: 2 },
                 { damage: 0, block: 0, healing: 0 },
-                { damage: 7, block: 0, healing: 2 },
-                { damage: 9, block: 3, healing: 0 },
-                { damage: 0, block: 0, healing: 6 },
-                { damage: 12, block: 0, healing: 0 },
+                { damage: 5, block: 0, healing: 2 },
+                { damage: 7, block: 2, healing: 0 },
+                { damage: 0, block: 0, healing: 4 },
+                { damage: 9, block: 0, healing: 0 },
             ], 5);
         }
 
